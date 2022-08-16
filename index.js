870aef788b252f4a68e01e0668fd73ae5142be3f
Sam Gotowka <sam.gotowka@gmail.com>
2022-08-16 14:34:29 -0400

    commit tree fe1b855257266f3cd5be6d4cfe14a15f53f88dce

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+function findMatching(drivers,dr){
+    return drivers.filter((el)=> el.toLowerCase()===dr.toLowerCase())
+  } 
+function fuzzyMatch(drivers,dr){
+    return drivers.filter((el)=>el.toLowerCase().indexOf(dr.toLowerCase()))=== 0
+    }
+
+function matchName(drivers){
+    for(const name of drivers) {
+        if(name=== drivers)
+        return matchName
+    }
+}  
